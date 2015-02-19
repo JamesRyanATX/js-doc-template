a7730305069c653e2187fb2aa8a502e982030dfd
Jeff Q <jqiu1998@yahoo.com>
2015-02-18 22:19:49 -0500

    Attempt to split the first block. Need to return output into a variable.

--- src/sample.rb
+++ src/sample.rb
@@ -1,4 +1,27 @@
 require 'json'
 
-list_of_things=File.read('sample-1.js')
-puts list_of_things
+original_file=File.read('sample-1.js')
+split_form=original_file.split('\n')
+x=1
+while x<=split_form.length do
+  
+  if ("/**"==split_form[x]) 
+    y=x+1
+    
+    while y<=split_form.length do
+     
+      if "}"==split_form[y]
+        block_one=split_form[x,y]
+      else
+        y+=1
+      end
+    
+    end
+  
+  else
+    x+=1
+  end
+
+end
+
+puts block_one
