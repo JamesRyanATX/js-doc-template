369b6a73865da681ce94b123392eb3395a1afa2e
Jeff Q <jqiu1998@yahoo.com>
2015-02-19 22:25:19 -0500

    Code is way to inefficient. Need to set each block variable to what the while loop returns. First while loop need to go hrough once

--- src/sample.rb
+++ src/sample.rb
@@ -1,27 +1,38 @@
 require 'json'
 
 original_file=File.read('sample-1.js')
-split_form=original_file.split('\n')
+split_form=original_file.split(" ")
+puts split_form
 x=1
 while x<=split_form.length do
-  
   if ("/**"==split_form[x]) 
     y=x+1
-    
-    while y<=split_form.length do
-     
+    while y<=split_form.length do     
       if "}"==split_form[y]
         block_one=split_form[x,y]
       else
         y+=1
       end
-    
     end
-  
   else
     x+=1
   end
-
 end
+x=1
+while x<=split_form.length do
+  if ("/**"==split_form[x]) 
+    y=x+1
+    while y<=split_form.length do     
+      if ";"==split_form[y]
+        variable=split_form[x,y]
+      else
+        y+=1
+      end
+    end
+  else
+    x+=1
+  end
+end
+
 
 puts block_one
