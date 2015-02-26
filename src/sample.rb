704da3f45dfae6f320db3948e8936345d63f150e
Jeff Q <jqiu1998@yahoo.com>
2015-02-25 21:21:51 -0500

    very crude and ugly documentation

--- src/sample.rb
+++ src/sample.rb
@@ -17,8 +17,29 @@ end
 # parsing the contents of each block to
 # see what type of code it was and how
 # we should format it.
+
 blocks.each_with_index do |block, index|
-  puts "Block #{index}:"
+  #puts "Block #{index}:"
   # Because split removes the /**, put it back
-  puts "/**" + block
+  #puts "/**" + block
 end
+
+split_blocks=blocks.map do |piece|
+  piece.split('*/')
+end
+
+puts"<html>"
+puts"<body>"
+puts"<h1>Documentation</h1>"
+puts"<p>"
+
+j=0
+while j<split_blocks.length do
+  puts split_blocks[j][1]
+  puts "<p>"
+  puts split_blocks[j][0]
+  puts "<p>"
+  j+=1
+end
+
+
